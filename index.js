b0a22559aaafa0437aa8cabb22531e5f1631e16c
Shahriar Fardows <shahriarfardows@gmail.com>
2024-07-26 04:40:46 +0600

    new code

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socket = require('socket.io');
+const io = socket(server);
 const port = 3000;
 
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  })
+  
+  
+// Start the server
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
